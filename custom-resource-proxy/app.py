b318abad1a733e6ed1696fcf077b4dfe90dfe20e
vaibhav.sinha <vaibhav.sinha@unacademy.com>
2022-06-05 23:36:31 +0530

    bugfix

--- custom-resource-proxy/app.py
+++ custom-resource-proxy/app.py
@@ -101,13 +101,13 @@ def callback(name):
     resp = requests.request(
         method=request.method,
         url=url,
-        headers={key: value for (key, value) in request.headers if key != 'Host'},
+        headers={key: value for (key, value) in request.headers if key != 'Host' and not key.startswith('X-')},
         data=request.get_data(),
         cookies=request.cookies,
         allow_redirects=False)
 
     excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
-    headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers and not name.lower().startswith('x-')]
+    headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers]
 
     response = Response(resp.content, resp.status_code, headers)
     return response
